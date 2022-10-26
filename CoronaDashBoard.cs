5e8c9478a8709a3e602f5c19e08f4985ddae1236
ilakkiya <ilak@gmail.com>
2022-10-26 10:35:31 +0530

    testadded

---
+++ CoronaDashBoard.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1basicsofcsharp
+{
+    internal class CoronaDashBoard
+    {
+        public List<Corona> listCorona = new List<Corona>();
+        public void addCoronaData(int code, String name, int active, int recorded, int death, int total)
+        {
+            //new Corona(code, name, active, recorded, death, total);
+            listCorona.Add(new Corona(code, name, active, recorded, death, total));
+
+        }
+        public void updateCoronaData(int code, String name, int active, int recorded, int death, int total)
+        {
+            Console.WriteLine("After Updation");
+            listCorona.Add(new Corona(code, name, active, recorded, death, total));
+        }
+        int ind = -1;
+        public List<int> a =new List<int>();
+        public void displayTopStates()
+        {
+            foreach(var rate in listCorona)
+            {
+               
+                a.Add( rate.death);
+              
+        
+                break;
+                
+            }
+            Console.WriteLine("Top death rate");
+            Console.WriteLine(a.Max());
+        }
+        public void deleteCorona(String name)
+        {
+          
+            Console.WriteLine("After Deleted");
+           
+            foreach(var dele in listCorona)
+            {
+                if (dele.name == name)
+                {
+                    ind = listCorona.IndexOf(dele);
+                    break;
+                }
+            }
+            listCorona.RemoveAt(ind);
+        }
+        public void listCoronaData()
+        {
+          
+            foreach (var item in listCorona)
+            {
+                Console.WriteLine(item.info());
+            }
+        }
+    }
+}
---
+++ CoronaTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1basicsofcsharp
+{
+    internal class CoronaTest
+    {
+        public static void ee()
+        {
+           
+            CoronaDashBoard dash = new CoronaDashBoard();
+            
+            dash.addCoronaData(40, "Maharashtra", 8000, 7000, 1000, 9000);
+            dash.addCoronaData(40, "Kerala", 6000, 4000, 900, 8000);
+            dash.addCoronaData(40, "Karnataka", 9000, 5000, 700, 6000);
+            dash.addCoronaData(40, "Tamil Nadu", 2000, 8000, 400, 4000);
+            dash.addCoronaData(40, "Andhra Pradesh", 4000, 7000, 200, 2000);
+
+            dash.listCoronaData();
+            dash.updateCoronaData(40,"Uttar Pradesh",2000,3000,200,3000);
+            dash.listCoronaData();
+            dash.deleteCorona("Kerala");
+            dash.listCoronaData();
+            dash.displayTopStates();
+            
+        }
+    }
+}
